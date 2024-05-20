4e9e8744af815e2f59f935abee362426a81ce56a
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-20 19:04:09 +0200

    aggiunto counter click e difficoltà

--- js/script.js
+++ js/script.js
@@ -1,37 +1,48 @@
 const playElement = document.querySelector('button');
+const gridElement = document.getElementById('grid');
+let clicks = 0;
 
 playElement.addEventListener('click', function(){
-    resetGame()
-    gridCreate()
-    bombsGenerator()
-})
-
-
-// Functions
-function resetGame(){
-    const reset = document.getElementById('grid');
-    reset.innerHTML = ''
-    return reset
-}
-
-function gridCreate(){
-    const gridElement = document.getElementById('grid');
+    resetGame(gridElement);
+    bombsGenerator();
     gridElement.classList.add('border');
 
     for(let i = 0; i < 100; i++){
-        let squareElement = document.createElement('article');
+        let squareElement = document.createElement('button');
         squareElement.classList.add('square');
         squareElement.classList.add('border');
         squareElement.classList.add('centered');
         squareElement.append(i +1);
-
+        
         squareElement.addEventListener('click', function(){
-            squareElement.classList.add('active');
+            clickME(squareElement);
+            
+            if {
+                squareElement.classList.add('bomb');
+            } else {
+                squareElement.classList.add('active');
+            }
             console.log(i +1);
         });
 
         gridElement.appendChild(squareElement);
     }
+
+})
+
+
+// Functions
+function resetGame(element){
+    let reset;
+    element.innerHTML = ''
+    return reset
+}
+
+
+function clickME(element) {
+    element.disabled = true;
+    clicks += 1;
+    document.getElementById('points').innerHTML = clicks;
 }
 
 function bombsGenerator(){
@@ -40,4 +51,5 @@ function bombsGenerator(){
         const randomBombNumber = Math.floor(Math.random() * 100) + 1;
         bombs.push(randomBombNumber);
     }
+    return bombs
 }
