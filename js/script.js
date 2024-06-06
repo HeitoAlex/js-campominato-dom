e8f5a584503b8c6f8d50a9beafc39e76a6e7ea6a
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-06-06 17:37:55 +0200

    bug fix su mode medium and hard

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const playElement = document.querySelector('button');
 const gridElement = document.getElementById('grid');
 const difficultySelectEl = document.getElementById('difficulty');
-const bombNumber = bombsGenerator();
+let bombNumber = [];
 let maxScore = 0;
 let clicks = 0;
 
-console.log(bombNumber);
+
 
 playElement.addEventListener('click', function(){
     
+    clicks = 0;
+    document.getElementById('points').innerHTML = clicks;
     resetGame(gridElement);
     let cells;
     let className;
@@ -29,7 +31,9 @@ playElement.addEventListener('click', function(){
             cells = 100;
             className = 'easy'; 
     }
-
+    
+    bombNumber = bombsGenerator(cells);
+    console.log(bombNumber);
     maxScore = cells - bombNumber.length;
     
     gridGenerator(gridElement, cells, className);
@@ -51,10 +55,10 @@ function clickME(element) {
     document.getElementById('points').innerHTML = clicks;
 }
 
-function bombsGenerator(){
+function bombsGenerator(maxRange){
     let bombs = [];
     while(bombs.length < 16) {
-        const randomBombNumber = Math.floor(Math.random() * 100) + 1;
+        const randomBombNumber = Math.floor(Math.random() * maxRange) + 1;
         if(!bombs.includes(randomBombNumber)) {
             bombs.push(randomBombNumber);
         }
@@ -80,9 +84,9 @@ function gridGenerator(gridElement, numberOfCells, classToAdd){
         squareElement.append(i +1);
         
         squareElement.addEventListener('click', function(){
-            clickME(squareElement);
             
-            if (bombNumber.includes(i)){
+            
+            if (bombNumber.includes(i+1)){
                 squareElement.classList.add('bomb');
                 // let allBombs = document.getElementsByClassName('bomb');
                 let allCells = document.getElementsByClassName('square');
@@ -95,6 +99,7 @@ function gridGenerator(gridElement, numberOfCells, classToAdd){
                 disableAll();
             } else {
                 squareElement.classList.add('active');
+                clickME(squareElement);
                 if(maxScore === clicks) {
                     disableAll();
                 }
